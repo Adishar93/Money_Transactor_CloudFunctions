853a5bea74cec85903b5c53eb1fc2a8a82cac144
adishar93 <adishar93@gmail.com>
2020-07-26 00:46:02 +0530

    WORKING: Notification received by the party to whom request is made

--- functions/index.js
+++ functions/index.js
@@ -6,14 +6,8 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-   exports.helloWorld = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello from Firebase!");
- });
-
-  exports.databaseListener = functions.database.ref('/Requests/{receiverId}/{senderId}')
+//The OnCreate Function
+  exports.sendNotificationOnCreate = functions.database.ref('/Requests/{receiverId}/{senderId}')
     .onCreate((snapshot, context) => {
       // Grab the current value of what was written to the Realtime Database.
       const request = snapshot.val();
@@ -23,10 +17,80 @@ admin.initializeApp();
       const getRetrieveTokenPromise = admin.database().ref('/DeviceTokens/'+ context.params.receiverId).once('value');
 
      return getRetrieveTokenPromise.then(dataSnapshot => {
-        const token=dataSnapshot.val();
-	console.log('Receiver Token', context.params.receiverId, token);
-        return null;
-        // Do something with the snapshot
+        const deviceToken=dataSnapshot.val();
+	console.log('Receiver Token', context.params.receiverId, deviceToken);
+        
+        // Send Notification to the respective device
+        var message = {
+  notification: {
+    title: 'New Request: '+request.name,
+    body: request.description
+  },
+  token: deviceToken
+};
+
+// Send a message to the device corresponding to the provided
+// registration token.
+return admin.messaging().send(message)
+  .then((response) => {
+    // Response is a message ID string.
+    console.log('Successfully sent Notification:', response);
+    return null;
+  })
+  .catch((error) => {
+    console.log('Error sending Notification:', error);
+  });
+
+
     })
     });
+
+
+
+//Similarly OnUpdate Function
+  exports.sendNotificationOnUpdate = functions.database.ref('/Requests/{receiverId}/{senderId}')
+    .onUpdate((change, context) => {
+
+     // Exit when the data is deleted.
+      if (!change.after.exists()) {
+        return null;
+      }
+      // Else Do these operations
+
+      // Grab the current value of what was written to the Realtime Database.
+      const request = change.after.val();
+      console.log('Retrieved Data', context.params.receiverId,     context.params.senderId, request.name,request.amount,request.description);
+      
+//Acquiring current Messaging token for the receiver of request
+      const getRetrieveTokenPromise = admin.database().ref('/DeviceTokens/'+ context.params.receiverId).once('value');
+
+     return getRetrieveTokenPromise.then(dataSnapshot => {
+        const deviceToken=dataSnapshot.val();
+	console.log('Receiver Token', context.params.receiverId, deviceToken);
+        
+        // Send Notification to the respective device
+        var message = {
+  notification: {
+    title: 'New Request: '+request.name,
+    body: request.description
+  },
+  token: deviceToken
+};
+
+// Send a message to the device corresponding to the provided
+// registration token.
+return admin.messaging().send(message)
+  .then((response) => {
+    // Response is a message ID string.
+    console.log('Successfully sent Notification:', response);
+    return null;
+  })
+  .catch((error) => {
+    console.log('Error sending Notification:', error);
+  });
+
+
+    })
+    });
+
 
