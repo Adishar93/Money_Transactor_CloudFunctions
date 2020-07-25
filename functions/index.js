04510af81776bfddbc4ca98e062c3440c0e4cf09
adishar93 <adishar93@gmail.com>
2020-07-26 01:05:54 +0530

    Code optimised, only one function deployed for OnUpdate and OnCreate instead of two

--- functions/index.js
+++ functions/index.js
@@ -6,55 +6,30 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-//The OnCreate Function
-  exports.sendNotificationOnCreate = functions.database.ref('/Requests/{receiverId}/{senderId}')
-    .onCreate((snapshot, context) => {
-      // Grab the current value of what was written to the Realtime Database.
-      const request = snapshot.val();
-      console.log('Retrieved Data', context.params.receiverId,     context.params.senderId, request.name,request.amount,request.description);
-      
-//Acquiring current Messaging token for the receiver of request
-      const getRetrieveTokenPromise = admin.database().ref('/DeviceTokens/'+ context.params.receiverId).once('value');
 
-     return getRetrieveTokenPromise.then(dataSnapshot => {
-        const deviceToken=dataSnapshot.val();
-	console.log('Receiver Token', context.params.receiverId, deviceToken);
+
+//OnWrite Function (here handling OnUpdate and OnCreate 	)
+  exports.sendNotificationOnUpdateNOnCreate = functions.database.ref('/Requests/{receiverId}/{senderId}')
+    .onWrite((change, context) => {
+
+     // Log according to OnUpdate or OnCreate
+      if (!change.before.exists()) {
+         console.log('OnCreate Executing');
+     
+      }
+      else {
         
-        // Send Notification to the respective device
-        var message = {
-  notification: {
-    title: 'New Request: '+request.name,
-    body: request.description
-  },
-  token: deviceToken
-};
-
-// Send a message to the device corresponding to the provided
-// registration token.
-return admin.messaging().send(message)
-  .then((response) => {
-    // Response is a message ID string.
-    console.log('Successfully sent Notification:', response);
-    return null;
-  })
-  .catch((error) => {
-    console.log('Error sending Notification:', error);
-  });
-
-
-    })
-    });
-
-
-
-//Similarly OnUpdate Function
-  exports.sendNotificationOnUpdate = functions.database.ref('/Requests/{receiverId}/{senderId}')
-    .onUpdate((change, context) => {
-
-     // Exit when the data is deleted.
+       // Exit when the data is deleted.
       if (!change.after.exists()) {
         return null;
       }
+      else {
+         console.log('OnUpdate Executing');
+      }
+
+      }
+
+     
       // Else Do these operations
 
       // Grab the current value of what was written to the Realtime Database.
@@ -71,7 +46,7 @@ return admin.messaging().send(message)
         // Send Notification to the respective device
         var message = {
   notification: {
-    title: 'New Request: '+request.name,
+    title: 'New Request From: '+request.name,
     body: request.description
   },
   token: deviceToken
