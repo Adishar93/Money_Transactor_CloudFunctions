16f71fae294bc7f3dcebc20d72842a07ce021560
adishar93 <adishar93@gmail.com>
2020-07-25 23:02:52 +0530

    FCM Device Token from Database now accessible to the cloud function

--- functions/index.js
+++ functions/index.js
@@ -3,8 +3,8 @@ const functions = require('firebase-functions');
 
 
 // The Firebase Admin SDK to access Cloud Firestore.
-//const admin = require('firebase-admin');
-//admin.initializeApp();
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -13,11 +13,20 @@ const functions = require('firebase-functions');
    response.send("Hello from Firebase!");
  });
 
-  exports.databaseListener = functions.database.ref('/Requests/{pushId}')
+  exports.databaseListener = functions.database.ref('/Requests/{receiverId}/{senderId}')
     .onCreate((snapshot, context) => {
       // Grab the current value of what was written to the Realtime Database.
       const request = snapshot.val();
-      console.log('Retrieved Data', context.params.pushId, request);
+      console.log('Retrieved Data', context.params.receiverId,     context.params.senderId, request.name,request.amount,request.description);
       
+//Acquiring current Messaging token for the receiver of request
+      const getRetrieveTokenPromise = admin.database().ref('/DeviceTokens/'+ context.params.receiverId).once('value');
+
+     return getRetrieveTokenPromise.then(dataSnapshot => {
+        const token=dataSnapshot.val();
+	console.log('Receiver Token', context.params.receiverId, token);
+        return null;
+        // Do something with the snapshot
+    })
     });
 
